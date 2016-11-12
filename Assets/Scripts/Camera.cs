0513ecf1f79d1f89c4fc04a89588269b4d120cc4
Plompk <mgrimshaw1990@gmail.com>
2016-11-12 01:49:09 +0000

    Moved shit

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+    public static Camera main { get; internal set; }
+
+    private Vector3 cameraTarget;
+
+    private Transform target;
+    // Use this for initialization
+    void Start ()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cameraTarget = new Vector3(target.position.x, transform.position.y, target.position.z - 2);
+        transform.position = Vector3.Lerp(transform.position, cameraTarget, Time.deltaTime * 8);
+    }
+}
